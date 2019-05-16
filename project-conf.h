2fd0eb5536422b9c216e0adc0db26dc44cc16939
DevTraleski <dev.traleski@gmail.com>
2019-05-16 17:39:00 -0300

    Init commit

--- project-conf.h
+++ project-conf.h
@@ -36,63 +36,9 @@
  *      Matthias Kovatsch <kovatsch@inf.ethz.ch>
  */
 
-#ifndef __PROJECT_ERBIUM_CONF_H__
-#define __PROJECT_ERBIUM_CONF_H__
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
 
-/* Custom channel and PAN ID configuration for your project. */
-/*
-   #undef RF_CHANNEL
-   #define RF_CHANNEL                     26
+#define LOG_LEVEL_APP LOG_LEVEL_DBG
 
-   #undef IEEE802154_CONF_PANID
-   #define IEEE802154_CONF_PANID          0xABCD
- */
-
-/* IP buffer size must match all other hops, in particular the border router. */
-/*
-   #undef UIP_CONF_BUFFER_SIZE
-   #define UIP_CONF_BUFFER_SIZE           256
- */
-
-/* Disabling RDC and CSMA for demo purposes. Core updates often
-   require more memory. */
-/* For projects, optimize memory and enable RDC and CSMA again. */
-#undef NETSTACK_CONF_RDC
-#define NETSTACK_CONF_RDC              nullrdc_driver
-
-/* Disabling TCP on CoAP nodes. */
-#undef UIP_CONF_TCP
-#define UIP_CONF_TCP                   0
-
-#undef NETSTACK_CONF_MAC
-#define NETSTACK_CONF_MAC     nullmac_driver
-
-/* Increase rpl-border-router IP-buffer when using more than 64. */
-#undef REST_MAX_CHUNK_SIZE
-#define REST_MAX_CHUNK_SIZE            48
-
-/* Estimate your header size, especially when using Proxy-Uri. */
-/*
-   #undef COAP_MAX_HEADER_SIZE
-   #define COAP_MAX_HEADER_SIZE           70
- */
-
-/* Multiplies with chunk size, be aware of memory constraints. */
-#undef COAP_MAX_OPEN_TRANSACTIONS
-#define COAP_MAX_OPEN_TRANSACTIONS     4
-
-/* Must be <= open transactions, default is COAP_MAX_OPEN_TRANSACTIONS-1. */
-/*
-   #undef COAP_MAX_OBSERVERS
-   #define COAP_MAX_OBSERVERS             2
- */
-
-/* Filtering .well-known/core per query can be disabled to save space. */
-#undef COAP_LINK_FORMAT_FILTERING
-#define COAP_LINK_FORMAT_FILTERING     0
-#undef COAP_PROXY_OPTION_PROCESSING
-#define COAP_PROXY_OPTION_PROCESSING   0
-
-/* Enable client-side support for COAP observe */
-#define COAP_OBSERVE_CLIENT 1
-#endif /* __PROJECT_ERBIUM_CONF_H__ */
+#endif /* PROJECT_CONF_H_ */
---
+++ server.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include "contiki.h"
+#include "contiki-lib.h"
+#include "contiki-net.h"
+#include "coap-engine.h"
+#include "coap-blocking-api.h"
+#include "net/ipv6/multicast/uip-mcast6.h"
+
+#include "sys/log.h"
+#include "coap-log.h"
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_APP
+
+#define DEBUG DEBUG_PRINT
+#include "net/ipv6/uip-debug.h"
+#include "net/routing/routing.h"
+
+//CoAP configuration
+
+//#define SERVER_EP "coap://[fe80::201:1:1:1]"
+#define SERVER_EP "coap://[ff03::fc]"
+#define TOGGLE_INTERVAL 10
+
+
+//Multicast configuration
+/* Start sending messages START_DELAY secs after we start so that routing can
+ * converge */
+#define START_DELAY 60
+#define MCAST_SINK_UDP_PORT 3001 /* Host byte order */
+#define SEND_INTERVAL CLOCK_SECOND /* clock ticks */
+#define ITERATIONS 100 /* messages */
+#define MAX_PAYLOAD_LEN 120
+
+
+extern coap_resource_t
+  res_hello;
+
+static struct etimer et;
+
+void
+client_chunk_handler(coap_message_t *response)
+{
+  const uint8_t *chunk;
+
+  int len = coap_get_payload(response, &chunk);
+
+  printf("|%.*s", len, (char *)chunk);
+}
+
+PROCESS(client, "CoAP Client");
+PROCESS(server, "CoAP Server");
+AUTOSTART_PROCESSES(&server, &client);
+
+PROCESS_THREAD(server, ev, data)
+{
+  PROCESS_BEGIN();
+
+  LOG_INFO("Starting Gateway CoAP Server\n");
+  coap_activate_resource(&res_hello, "test/hello");
+
+  PRINTF("Multicast Engine Starting\n");
+  NETSTACK_ROUTING.root_start();
+
+  while(1) {
+    PROCESS_WAIT_EVENT();
+  }
+
+  PROCESS_END();
+}
+
+PROCESS_THREAD(client, ev, data)
+{
+  static coap_endpoint_t server_ep;
+  PROCESS_BEGIN();
+
+  static coap_message_t request[1];      /* This way the packet can be treated as pointer as usual. */
+
+  coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
+
+  etimer_set(&et, START_DELAY * CLOCK_SECOND);
+
+  while(1) {
+    PROCESS_YIELD();
+
+    if(etimer_expired(&et)) {
+      printf("--Send--\n");
+
+      /* prepare request, TID is set by COAP_BLOCKING_REQUEST() */
+      coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
+      coap_set_header_uri_path(request, "test/hello");
+
+      const char msg[] = "Oh hi mark!";
+
+      coap_set_payload(request, (uint8_t *)msg, sizeof(msg) - 1);
+
+      LOG_INFO_COAP_EP(&server_ep);
+      LOG_INFO_("\n");
+
+      COAP_BLOCKING_REQUEST(&server_ep, request, client_chunk_handler);
+
+      printf("\n--Done--\n");
+
+      etimer_set(&et, TOGGLE_INTERVAL * CLOCK_SECOND);
+    }
+  }
+
+  PROCESS_END();
+}
